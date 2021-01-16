ab07f9d5c26aeef3fb23af7d00ba4da5c928430e
Jose <joey_manuel@outlook.com>
2021-01-16 09:16:13 -0500

    9. Finished Level 3
    Finished level 3:
    
    Created WrongWayHandler.cs file

---
+++ Puzzled-Ball/Assets/Scripts/Management Script/WrongWayHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WrongWayHandler : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Ball")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        
+    }
+
+
+
+} // WrongWayHandler class
