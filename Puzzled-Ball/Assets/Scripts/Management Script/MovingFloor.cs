834ff732fdb6d13ffbb9705dc0f44e184429dded
Jose <joey_manuel@outlook.com>
2021-01-17 10:58:54 -0500

    14. Level 6 finished
    File MovingFloor.cs created.
    
    Game is finished and ready to merge.

---
+++ Puzzled-Ball/Assets/Scripts/Management Script/MovingFloor.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingFloor : MonoBehaviour
+{
+    #region Variables
+    [SerializeField]
+    private bool moveX, moveY, moveZ;
+
+    [SerializeField]
+    private float speed;
+
+    #endregion Variables
+
+
+    #region UnityFunctions
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlatform();
+    }
+
+    #endregion UnityFunctions
+
+    void MovePlatform()
+    {
+        if(moveX)
+        {
+            Vector3 temp = transform.position;
+            temp.x += speed * Time.deltaTime;
+            transform.position = temp;
+        }
+
+        if(moveY)
+        {
+            Vector3 temp = transform.position;
+            temp.y += speed * Time.deltaTime;
+            transform.position = temp;
+        }
+
+        if(moveZ)
+        {
+            Vector3 temp = transform.position;
+            temp.z += speed * Time.deltaTime;
+            transform.position = temp;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Floor")
+        {
+            speed *= -1;
+        }
+    }
+
+
+
+} // MovingFloor class
+
