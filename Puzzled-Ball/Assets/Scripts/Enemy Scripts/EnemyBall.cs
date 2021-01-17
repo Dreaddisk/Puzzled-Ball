8626e9561dcc1aa5bc1d4c55d22c56cbf35218bc
Jose <joey_manuel@outlook.com>
2021-01-17 10:45:48 -0500

    10. Started and Finished Level 4
    Level 4 already finished.
    files EnemyBallTrigger.cs EnemyBall.cs created.

---
+++ Puzzled-Ball/Assets/Scripts/Enemy Scripts/EnemyBall.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBall : MonoBehaviour
+{
+    #region Variables
+    private Transform ballTarget;
+    private Vector3 ballPositionDirection;
+
+    private bool canAttack, readyToAttack;
+
+    [HideInInspector]
+    public bool stunned;
+
+    private Rigidbody myBody;
+    private RaycastHit ballHit;
+
+    #endregion Varaibles
+
+
+
+
+    #region UnityFunctions
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        CheckIfCanAttack();
+    }
+
+    private void FixedUpdate()
+    {
+        Attack();
+    }
+
+    #endregion UnityFunctions
+
+    void GetBallTarget(Transform target)
+    {
+        ballTarget = target;
+    }
+
+    void CanAttackToggle(bool canAttack)
+    {
+        this.canAttack = canAttack;
+    }
+
+    void CheckIfCanAttack()
+    {
+        if(canAttack && !stunned &&(myBody.velocity.sqrMagnitude <= 0.11f))
+        {
+            ballPositionDirection = ballTarget.position - transform.position;
+
+            if(Physics.Raycast(transform.position, ballPositionDirection, out ballHit, 25))
+            {
+                if(ballHit.transform.tag == "Ball")
+                {
+                    readyToAttack = true;
+                }
+            }
+        }
+    }
+
+
+    void Attack()
+    {
+        if(readyToAttack)
+        {
+            myBody.AddForce(ballPositionDirection * 200f);
+            readyToAttack = false;
+        }
+    }
+
+
+
+} // EnemyBall class
