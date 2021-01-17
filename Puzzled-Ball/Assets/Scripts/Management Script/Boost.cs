1a59102fde39a12eaa4be6635b4f5de2302b7f26
Jose <joey_manuel@outlook.com>
2021-01-17 10:50:43 -0500

    12. Level 5 finished
    Boost.cs file created

---
+++ Puzzled-Ball/Assets/Scripts/Management Script/Boost.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boost : MonoBehaviour
+{
+    public float force = 150f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Ball")
+        {
+            other.gameObject.GetComponent<Rigidbody>().AddForce
+                (transform.forward * -force, ForceMode.Impulse);
+        }
+    }
+}
